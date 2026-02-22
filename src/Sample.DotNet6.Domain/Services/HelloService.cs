e6e807fd5a611b40d00a5ee3ce6f5b3be7ca5ac4
muhammad chaerul hafiz <mchaerulhafiz@gmail.com>
2026-02-22 08:59:28 +0700

    Revert "Add API keys and insecure HTTP request in HelloService"
    This reverts commit b340eb9317028bf7bf082c0e7a0e0064aa76e3be.

--- src/Sample.DotNet6.Domain/Services/HelloService.cs
+++ src/Sample.DotNet6.Domain/Services/HelloService.cs
@@ -11,14 +11,6 @@
         {
             var hello = $"Hello";
 
-            private const string ApiKey = "12345-SECRET-KEY";
-    private const string AnuKey = "12312313213-anu-secret-KEY";
-    using var client = new HttpClient();
-var response = await client.GetStringAsync("http://example.com/api"); // insecure
- string sql = $"DELETE FROM Users WHERE Name='{username}'"; 
-    string s = null;
-    int length = s.Length;  // p
-
             if (isHappy)
                 return $"{hello}, you seem to be happy today";
             return hello;
